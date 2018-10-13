470138fd164ff94dd5bf2dcfb87046dccef93311
Micheal Zamayeri <omz85i@gmail.com>
2018-10-13 12:23:49 +0100

    Update Database/Import MYSQL.py

--- Database/Import MYSQL.py
+++ Database/Import MYSQL.py
@@ -77,7 +77,10 @@ cnx.close()
 
 #####   Read raw extract with pandas and import to MYSQL
 df = pd.read_csv("Data/df_rawConcat.csv")
+
+#####	Validate data types
 print (df.dtypes)
 
+#####	Use SQL Alchemy to create a connector to MYSQL and use pandas to import data.
 engine = create_engine('mysql+mysqlconnector://root:my-secret-pw@localhost/PythonToMysql')
 df.to_sql(name='TP Framework',con=engine,index=False,if_exists='append')
